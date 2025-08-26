388f6af45b1fa1367de55ba34346cf4fc0147772
Eleni <thelittlemermaid894@gmail.com>
2025-08-26 15:06:02 -0400

    ratatouille img carousel

--- script.js
+++ script.js
@@ -7,3 +7,32 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
 }
+
+// RATATOUILLE CAROUSEL
+const slides = document.querySelector('.carousel_imgs');
+const images = document.querySelectorAll('.carousel_imgs img');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+const container = document.querySelector('.carousel');
+
+let index = 0;
+
+function getSlideWidth() {
+  return container.clientWidth;
+}
+
+function showSlide(i) {
+  if (i < 0) {
+    index = images.length - 1;
+  } else if (i >= images.length) {
+    index = 0;
+  } else {
+    index = i;
+  }
+  slides.style.transform = `translateX(${-index * getSlideWidth()}px)`;
+}
+
+prevBtn.addEventListener('click', () => showSlide(index - 1));
+nextBtn.addEventListener('click', () => showSlide(index + 1));
+
+setInterval(() => showSlide(index + 1), 3000);
